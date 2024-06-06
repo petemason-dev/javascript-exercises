67c5d54757ffb53bdecb180a1a33b8a2f8ca663b
Pete Mason <pete.dev@robot9.uk>
2024-06-06 08:24:20 +0100

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (inputArray, ...toRemove) {
+  return inputArray.filter((item) => !toRemove.includes(item));
 };
 
 // Do not edit below this line
